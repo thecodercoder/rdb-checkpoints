87eea82246dce9882cbcfb7d1c73ec6364825f8b
thecodercoder <37986728+thecodercoder@users.noreply.github.com>
2024-05-25 21:55:52 -0500

    RDB End of Course

---
+++ 03_Ultimate_Course/End of Course/main.js
@@ -0,0 +1,61 @@
+import './app/scss/style.scss';
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from 'body-scroll-lock';
+
+const main = document.querySelector('#main');
+const footer = document.querySelector('#footer');
+const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
+const menuTopNav = document.querySelector('#menuTopNav');
+const overlay = document.querySelector('#overlay');
+const breakpoint = window.matchMedia('(width < 43.75em)');
+
+const setupTopNav = () => {
+  if (breakpoint.matches) {
+    // console.log('is mobile');
+    menuTopNav.setAttribute('inert', '');
+  } else {
+    // console.log('is tablet/desktop');
+    menuTopNav.removeAttribute('inert');
+  }
+};
+
+setupTopNav();
+
+btnOpen.addEventListener('click', openMobileMenu);
+btnClose.addEventListener('click', closeMobileMenu);
+
+breakpoint.addEventListener('change', () => {
+  // console.log('breakpoint crossed');
+  setupTopNav();
+});
+
+function openMobileMenu() {
+  // console.log('run openMobileMenu');
+  btnOpen.setAttribute('aria-expanded', 'true');
+  main.setAttribute('inert', '');
+  footer.setAttribute('inert', '');
+  menuTopNav.removeAttribute('inert');
+  menuTopNav.style.transitionDuration = '400ms';
+  overlay.style.transitionDuration = '400ms';
+  disableBodyScroll(menuTopNav);
+  btnClose.focus();
+}
+
+function closeMobileMenu() {
+  // console.log('run closeMobileMenu');
+  btnOpen.setAttribute('aria-expanded', 'false');
+  main.removeAttribute('inert');
+  footer.removeAttribute('inert');
+  menuTopNav.setAttribute('inert', '');
+  enableBodyScroll(menuTopNav);
+  btnOpen.focus();
+
+  setTimeout(() => {
+    menuTopNav.removeAttribute('style');
+    overlay.removeAttribute('style');
+  }, 500);
+}
---
+++ 03_Ultimate_Course/End of Course/vite.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+export default {
+  css: {
+    devSourcemap: true,
+  },
+};
